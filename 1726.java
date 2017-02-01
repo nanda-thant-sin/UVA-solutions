dfd90365f9b1f0362aefe03bc1faa15c8b5fd62d
nanda-thant-sin <jacepot@gmail.com>
2017-02-01 18:24:32 +0630

    Update 1726.java

--- 1726.java
+++ 1726.java
@@ -7,29 +7,27 @@ class Main
 		for(int i=1;i<=test;i++){
 			int ques=sc.nextInt();
 		    int[] rank=new int[10];
-			int[] leak=new int[ques];
+			int[] leak=new int[10];
 			int r;
 			int c;
 			char a;
-			int index=0;
 			for(int j=0;j<ques;j++){
 				r=sc.nextInt();
 				c=sc.nextInt();
 				a=sc.next().charAt(0);
 				if(c==1 && a=='c')
-					leak[index++]=r;
+					leak[r-1]++;
 				else if(c==0 && a=='i')
 					rank[r-1]++;
 			}
-			Arrays.sort(leak);
 			int k=0;
 			int current=0;
 			int res=0;
-			for(int j=ques-index;j<ques;j++){
-				while(k+1<leak[j]){
+			for(int j=0;j<10;j++){
+				while(k<j){
 					current+=rank[k++];
 				}
-				res+=current;
+				res+=current*leak[j];
 			}
 			System.out.println(res);
 		}
