58ef5705f869e2f00da9f0bb4bf3ea5cbdeab200
nanda-thant-sin <jacepot@gmail.com>
2018-01-31 14:52:38 +0000

    Create 10633 - Rare Easy Problem.java

---
+++ import java.util.- import java.lang.- import java.io.-  class Main - -public static void main-String- args- - -Scanner sc - new Scanner-System.in- -while -true- - -long n - sc.nextLong- -if -n - 0- -break- -if -n - 9 - 0- - -System.out.println-n - n/9 - 1- - - - - -n - n/9- - else - -System.out.println-n - n/10633 - Rare Easy Problem.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		while (true) {
+			long n = sc.nextLong();
+			if (n == 0)
+				break;
+			if (n % 9 == 0) {
+				System.out.println((n + n / 9 - 1) + " " + (n + n / 9));
+			} else {
+				System.out.println(n + n / 9);
+			}
+		}
+
+	}
+
+}
