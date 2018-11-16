d1ca4eea5c621d27dedef0ea4c4de544776a92a5
nanda-thant-sin <jacepot@gmail.com>
2018-11-16 12:20:50 +0630

    Create 10664 - Luggage.java

---
+++ 10664 - Luggage.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class Main {
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		for (int q = 0; q < t; q++) {
+			String[] ss = br.readLine().split(" ");
+			int n = ss.length;
+			int[] wi = new int[n + 1];
+			int[] p = new int[n + 1];
+			int w = 0;
+			for (int i = 1; i <= n; i++) {
+				p[i] = Integer.parseInt(ss[i - 1]);
+				wi[i] = Integer.parseInt(ss[i - 1]);
+				w += p[i];
+			}
+			if (w % 2 != 0) {
+				sb.append("NO\n");
+			} else {
+				w /= 2;
+				int[][] item_weight = new int[n + 1][w + 1];
+				for (int i = 1; i <= n; i++) {
+					for (int j = 1; j <= w; j++) {
+						if (j >= wi[i]) {
+							item_weight[i][j] = Math.max(p[i] + item_weight[i - 1][j - wi[i]], item_weight[i - 1][j]);
+						} else {
+							item_weight[i][j] = item_weight[i - 1][j];
+						}
+					}
+				}
+				if (item_weight[n][w] == w) {
+					sb.append("YES\n");
+				} else {
+					sb.append("NO\n");
+				}
+			}
+		}
+		System.out.println(sb);
+		br.close();
+	}
+}
