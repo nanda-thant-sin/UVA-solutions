c6dd2f8d4a80395d77f61a16d077e86e3e70adb1
nanda-thant-sin <jacepot@gmail.com>
2019-01-01 10:55:42 +0630

    Update 369 - Combinations.java

--- 369 - Combinations.java
+++ 369 - Combinations.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		BigInteger[] data = new BigInteger[101];
-		data[0] = data[1] = new BigInteger("1");
+		data[1] = new BigInteger("1");
 		for (int i = 2; i < 101; i++) {
 			data[i] = new BigInteger(i + "").multiply(data[i - 1]);
 		}
